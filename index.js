28d6a794f11110546f6de0fef18fe467a2e2e3fa
Souvik Brahma <jitbrahma@gmail.com>
2018-07-01 15:06:01 +0530

    heroku commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const http = require('http');
-const hostname = 'https://anoclub.herokuapp.com/';
-const port = process.env.PORT || 8080;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
