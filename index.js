55640563de03e1c6b93588422e7c5a4c6a137dc2
Souvik Brahma <jitbrahma@gmail.com>
2018-07-01 14:14:36 +0530

    heroku commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const hostname = '127.0.0.1';
-const PORT = process.env.PORT || 8080;
+const post = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
